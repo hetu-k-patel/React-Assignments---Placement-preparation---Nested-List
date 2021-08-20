937ff2d3b69782d98769f864e56bbffdc702aef0
hetu-k-patel <77904891+hetu-k-patel@users.noreply.github.com>
2021-08-20 19:37:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,210 +1,174 @@
-import React, { Component, useState } from 'react';
-import './../styles/App.css';
+import React, { Component, useState } from "react";
+import "./../styles/App.css";
+import State from "./State";
 
 // Do not alter the states const and values inside it.
 const states = [
-   {
-      name: 'Madhya Pradesh',
-      cities: [
-         {
-            name: 'Indore',
-            towns: [
-               {
-                  name: 'Mhow',
-               },
-               {
-                  name: 'Dewas',
-               },
-            ],
-         },
-         {
-            name: 'Bhopal',
-            towns: [
-               {
-                  name: 'Manit',
-               },
-               {
-                  name: 'Berasia',
-               },
-            ],
-         },
-         {
-            name: 'Gwalior',
-            towns: [
-               {
-                  name: 'Ajaypur',
-               },
-            ],
-         },
-      ],
-   },
-   {
-      name: 'Jharkhand',
-      cities: [
-         {
-            name: 'Dhanbad',
-            towns: [
-               {
-                  name: 'IIT(ISM) Dhanbad',
-               },
-               {
-                  name: 'Hirapur',
-               },
-            ],
-         },
-         {
-            name: 'Wasseypur',
-            towns: [
-               {
-                  name: "Sardar khan's",
-               },
-               {
-                  name: "Faizal khan's",
-               },
-            ],
-         },
-         {
-            name: 'Mirzapur',
-            towns: [
-               {
-                  name: "Kaleen bhaiya's",
-               },
-               {
-                  name: "Guddu bhaiya's",
-               },
-            ],
-         },
-      ],
-   },
-   {
-      name: 'Assam',
-      cities: [
-         {
-            name: 'Guwhati',
-            towns: [
-               {
-                  name: 'Amin',
-               },
-               {
-                  name: 'Jalah',
-               },
-            ],
-         },
-         {
-            name: 'Jungle1',
-            towns: [
-               {
-                  name: 'Tiger found at IIT Guwahati',
-               },
-               {
-                  name: 'Leopard found in IIT Guwahati',
-               },
-            ],
-         },
-         {
-            name: 'Tezpur',
-            towns: [
-               {
-                  name: 'Dibrugarh',
-               },
-               {
-                  name: 'Silchar',
-               },
-            ],
-         },
-      ],
-   },
-   {
-      name: 'Bihar',
-      cities: [
-         {
-            name: 'Patna',
-            towns: [
-               {
-                  name: 'Sonpur',
-               },
-               {
-                  name: 'Maner',
-               },
-            ],
-         },
-         {
-            name: 'Gaya',
-            towns: [
-               {
-                  name: 'Bakraur',
-               },
-               {
-                  name: 'Barachatti',
-               },
-            ],
-         },
-         {
-            name: 'Darbhanga',
-            towns: [
-               {
-                  name: 'Singhwara',
-               },
-               {
-                  name: 'Jale',
-               },
-            ],
-         },
-      ],
-   },
+  {
+    name: "Madhya Pradesh",
+    cities: [
+      {
+        name: "Indore",
+        towns: [
+          {
+            name: "Mhow"
+          },
+          {
+            name: "Dewas"
+          }
+        ]
+      },
+      {
+        name: "Bhopal",
+        towns: [
+          {
+            name: "Manit"
+          },
+          {
+            name: "Berasia"
+          }
+        ]
+      },
+      {
+        name: "Gwalior",
+        towns: [
+          {
+            name: "Ajaypur"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Jharkhand",
+    cities: [
+      {
+        name: "Dhanbad",
+        towns: [
+          {
+            name: "IIT(ISM) Dhanbad"
+          },
+          {
+            name: "Hirapur"
+          }
+        ]
+      },
+      {
+        name: "Wasseypur",
+        towns: [
+          {
+            name: "Sardar khan's"
+          },
+          {
+            name: "Faizal khan's"
+          }
+        ]
+      },
+      {
+        name: "Mirzapur",
+        towns: [
+          {
+            name: "Kaleen bhaiya's"
+          },
+          {
+            name: "Guddu bhaiya's"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Assam",
+    cities: [
+      {
+        name: "Guwhati",
+        towns: [
+          {
+            name: "Amin"
+          },
+          {
+            name: "Jalah"
+          }
+        ]
+      },
+      {
+        name: "Jungle1",
+        towns: [
+          {
+            name: "Tiger found at IIT Guwahati"
+          },
+          {
+            name: "Leopard found in IIT Guwahati"
+          }
+        ]
+      },
+      {
+        name: "Tezpur",
+        towns: [
+          {
+            name: "Dibrugarh"
+          },
+          {
+            name: "Silchar"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Bihar",
+    cities: [
+      {
+        name: "Patna",
+        towns: [
+          {
+            name: "Sonpur"
+          },
+          {
+            name: "Maner"
+          }
+        ]
+      },
+      {
+        name: "Gaya",
+        towns: [
+          {
+            name: "Bakraur"
+          },
+          {
+            name: "Barachatti"
+          }
+        ]
+      },
+      {
+        name: "Darbhanga",
+        towns: [
+          {
+            name: "Singhwara"
+          },
+          {
+            name: "Jale"
+          }
+        ]
+      }
+    ]
+  }
 ];
 
 function App() {
-   const handleClick = (e) => {
-      document.querySelectorAll('li').forEach((el) => {
-         const id = el.id;
-         if (id && !id.includes('state')) {
-            el.removeAttribute('id');
-         }
-      });
-
-      e.target.children[0].childNodes.forEach((node, index) => {
-         if (node.classList.contains('city')) {
-            if (!node.id) {
-               node.id = `city${index + 1}`;
-            } else node.removeAttribute('id');
-         } else if (node.classList.contains('town')) {
-            if (!node.id) {
-               node.id = `town${index + 1}`;
-            } else node.removeAttribute('id');
-         }
-      });
-
-      e.target.children[0].classList.toggle('visible');
-   };
-
-   const handleClick1 = (e) => {};
-
-   return (
-      <div id="main">
-         <ul>
-            {states.map((state, index) => (
-               <li key={'state' + index} id={'state' + (index + 1)} onClick={handleClick}>
-                  {state.name}
-                  <ul>
-                     {state.cities.map((city, index) => (
-                        <li className="city" key={'city' + index} onClick={handleClick1}>
-                           {city.name}
-                           <ul>
-                              {city.towns.map((town, index) => (
-                                 <li className="town" key={'town' + index}>
-                                    {town.name}
-                                 </li>
-                              ))}
-                           </ul>
-                        </li>
-                     ))}
-                  </ul>
-               </li>
-            ))}
-         </ul>
-      </div>
-   );
+  return (
+    <div id="main">
+      <ul>
+        {states.map((item) => (
+          <State
+            index={states.indexOf(item)}
+            name={item.name}
+            cities={item.cities}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
